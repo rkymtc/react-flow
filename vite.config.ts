7b765aaff2e3393e58e1232c26c8e55aebb7e417
rkymtc <76444299+rkymtc@users.noreply.github.com>
2025-01-26 11:18:00 +0300

    edits port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-/// <reference types="vite/client" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-}) 
+  server: {
+    port: 3000,
+    open: true, // Tarayıcıyı otomatik açar
+  },
+})
